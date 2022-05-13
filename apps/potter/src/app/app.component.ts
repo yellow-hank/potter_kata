0a7508abbe1c7b56a8aa80fed013f218ccb01fcc
yellowhank <shps97040608@gmail.com>
2022-05-13 11:31:14 +0800

    initial commit

---
+++ apps/potter/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'potter-kata-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'potter';
+}
