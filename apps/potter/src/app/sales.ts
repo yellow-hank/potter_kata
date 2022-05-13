48ee57d43ef81f01d76de24adf841b5e8472fd9c
yellowhank <shps97040608@gmail.com>
2022-05-13 15:11:35 +0800

    format: fix format

--- apps/potter/src/app/sales.ts
+++ apps/potter/src/app/sales.ts
@@ -6,20 +6,20 @@ export class Sales {
     let diff = 0;
     let change = 1;
     const count = [];
-    const discount = [1,1,0.95,0.9,0.8,0.75];
-    for(let i = 0; i < 5; i++){
+    const discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
+    for (let i = 0; i < 5; i++) {
       count.push(0);
     }
 
-    for(let i = 0; i < orders.length;i++){
+    for (let i = 0; i < orders.length; i++) {
       count[orders[i]] += 1;
     }
 
-    while(change){
+    while (change) {
       change = 0;
       diff = 0;
-      for(let i = 0; i < 5; i++){
-        if(count[i] > 0){
+      for (let i = 0; i < 5; i++) {
+        if (count[i] > 0) {
           diff++;
           count[i] -= 1;
           change = 1;
@@ -27,10 +27,6 @@ export class Sales {
       }
       total += discount[diff] * diff * pricePerOne;
     }
-    
-
-
-
 
     return total;
   }
