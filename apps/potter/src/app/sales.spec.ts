f879c4d9662352175dc94752eb4a9eec78a384ab
yellowhank <shps97040608@gmail.com>
2022-05-13 15:10:32 +0800

    test: add edge test for discount

--- apps/potter/src/app/sales.spec.ts
+++ apps/potter/src/app/sales.spec.ts
@@ -32,4 +32,13 @@ describe('Sales', () => {
     expect(sales.price([0, 0, 1, 2, 2, 3])).toBe((8 * 4 * 0.8) + (8 * 2 * 0.95));
     expect(sales.price([0, 1, 1, 2, 3, 4])).toBe(8 + (8 * 5 * 0.75));
   });
+
+  test('testEdgeCases', () => {
+    expect(sales.price([0, 0, 1, 1, 2, 2, 3, 4])).toBe(2 * (8 * 4 * 0.8));
+    expect(
+      sales.price([
+        0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4,
+      ])
+    ).toBe(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8));
+  });
 });
